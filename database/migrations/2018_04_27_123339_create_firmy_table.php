e9c6248e29e94a4f52611bed8e8343242da250ff
kmediapl <lk@kmedia.pl>
2018-04-27 15:44:47 +0200

    1

---
+++ database/migrations/2018_04_27_123339_create_firmy_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFirmyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('firmy', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nazwa_firmy');
+            $table->intiger('nip');
+            $table->string('miejscowosc');
+            $table->string('kod_pocztowy');
+            $table->string('wojewodztwo');
+            $table->string('poczta');
+            $table->string('ulica');    
+            $table->string('nr_domu');
+            $table->string('nr_lokalu');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('firmy');
+    }
+}
---
+++ database/migrations/2018_04_27_124052_create_kasy_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKasyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kasy', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nazwa_kasy');
+            $table->string('nr_fabryczny');
+            $table->string('nr_unikatowy');
+            $table->intiger('typ_kasy_id');
+            $table->string('nazwa_kasy');
+            $table->string('nazwa_kasy');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kasy');
+    }
+}
