0127cb623730826e361befdf60fd1e7ac9b68915
Łukasz Kwiatkowski <lukaszkwiatkowski@MacBook-Air-ukasz.local>
2018-05-02 18:38:46 +0200

    12

--- config/app.php
+++ config/app.php
@@ -148,6 +148,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
+        niklasravnsborg\LaravelPdf\PdfServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -212,7 +213,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
       'Html' => Collective\Html\HtmlFacade::class,
-      'PDF' => Barryvdh\DomPDF\Facade::class,
+      'PDF' => niklasravnsborg\LaravelPdf\Facades\Pdf::class
 
     ],
 
